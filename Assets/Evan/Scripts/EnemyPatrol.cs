85cd76afaf655eea54b48f501305b8e1d4f5bd25
MUKILTEO\1707224 <1707224@Mukilteo.wednet.edu>
2021-09-14 10:24:25 -0700

    Update Materials.meta, Blue.mat, and 16 more files...

---
+++ Assets/Evan/Scripts/EnemyPatrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyPatrol : MonoBehaviour
+{
+
+   [SerializeField]
+   GameObject[] partolPoint = new GameObject[5];
+
+    int currentPoint = 0;
+
+    NavMeshAgent navA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navA = GetComponent<NavMeshAgent>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        navA.destination = partolPoint[currentPoint].transform.position;
+
+
+    }
+}
