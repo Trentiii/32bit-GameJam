575a632dda5204027de856a4b9eca8e213aa5ad4
MUKILTEO\1707234 <1707234@mukilteo.wednet.edu>
2021-09-15 10:24:40 -0700

    still working on movement

--- Assets/Eric/Scripts/PlayerMovement.cs
+++ Assets/Eric/Scripts/PlayerMovement.cs
@@ -21,10 +21,10 @@ public class PlayerMovement : MonoBehaviour
     {
         if (sameSpeed)
             sameSpeedMovement();
-        else
+        if(!sameSpeed)
             SpeedMovement();
     }
-
+    // this is for movement that isn't always equal (faster at angles)
     private void SpeedMovement()
     {
         xVel = Input.GetAxisRaw("Horizontal") * spd * Time.deltaTime;
@@ -32,18 +32,39 @@ public class PlayerMovement : MonoBehaviour
 
         this.transform.position += new Vector3(xVel, 0, zVel);
     }
-
+    // this is for movement that whichever way you go, you'll always go the same speed
     private void sameSpeedMovement()
     {
         xVel = Input.GetAxisRaw("Horizontal") * spd * Time.deltaTime;
         zVel = Input.GetAxisRaw("Vertical") * spd * Time.deltaTime;
 
-        float xVel2 = 3f;
-        float zVel2 = 3f;
+
+        float angle = Mathf.Atan2(xVel, zVel);
+        Debug.Log(angle);
+
+
+        float xVel2 = Mathf.Sin(angle);
+        float zVel2 = Mathf.Cos(angle);
 
 
 
         this.transform.position += new Vector3(xVel2, 0, zVel2);
+        //Debug.Log(new Vector3(xVel2, 0, zVel2));
     }
+    /*
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);      Wanted position
+        Vector3 direction = mousePosition - Player.transform.position;                     xxxxxxx
 
+        float angle = Mathf.Atan2(direction.y, direction.x);         angle
+        float b2 = Mathf.Sin(angle);
+        float a2 = Mathf.Cos(angle);
+
+        offset = new Vector3(a2, b2, -10f);
+        if (Peeking)
+        {
+            Vector3 desiredPosition = Player.transform.position + (lookLength * offset);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpd);
+            transform.position = smoothedPosition;
+        }
+    */
 }
