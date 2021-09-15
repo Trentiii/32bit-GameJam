a8668c51e73d696a92f4bae0c621aac0b68b761e
MUKILTEO\1707344 <1707344@mukilteo.wednet.edu>
2021-09-15 09:13:44 -0700

    Finished very basic Wanted level. When comes in contact with a trigger it shows as "seen"

---
+++ Assets/Gavin/Wanted/Scripts/WantedScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WantedScript : MonoBehaviour
+{
+    public Sprite hiddenImage;
+    public Sprite seenImage;
+    public Image wantedUI;
+
+    public float hiddenTime;
+
+    bool hidden = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wantedUI.sprite = hidden ? hiddenImage : seenImage;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print(1);
+        Seen();
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        print(4);
+    }
+
+    public void Seen()
+    {
+        hidden = false;
+        StartCoroutine("TurnOnHidden");
+        print(2);
+    }
+
+    IEnumerator TurnOnHidden()
+    {
+        yield return new WaitForSeconds(hiddenTime);
+        hidden = true;
+        print(3);
+        
+    }
+}
