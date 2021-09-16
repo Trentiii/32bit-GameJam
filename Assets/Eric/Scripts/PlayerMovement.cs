06adf5da525760a42048194627ff8f27b5c48289
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-09-15 20:47:02 -0700

    Finished movement -_-

--- Assets/Eric/Scripts/PlayerMovement.cs
+++ Assets/Eric/Scripts/PlayerMovement.cs
@@ -19,52 +19,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        xVel = Input.GetAxisRaw("Horizontal");
+        zVel = Input.GetAxisRaw("Vertical");
+
         if (sameSpeed)
-            sameSpeedMovement();
-        if(!sameSpeed)
-            SpeedMovement();
+            this.transform.position += new Vector3(xVel, 0, zVel).normalized * (spd * Time.deltaTime);
+        if (!sameSpeed)
+            this.transform.position += new Vector3(xVel, 0, zVel) * (spd * Time.deltaTime);
     }
-    // this is for movement that isn't always equal (faster at angles)
-    private void SpeedMovement()
-    {
-        xVel = Input.GetAxisRaw("Horizontal") * spd * Time.deltaTime;
-        zVel = Input.GetAxisRaw("Vertical")   * spd * Time.deltaTime;
-
-        this.transform.position += new Vector3(xVel, 0, zVel);
-    }
-    // this is for movement that whichever way you go, you'll always go the same speed
-    private void sameSpeedMovement()
-    {
-        xVel = Input.GetAxisRaw("Horizontal") * spd * Time.deltaTime;
-        zVel = Input.GetAxisRaw("Vertical") * spd * Time.deltaTime;
-
-
-        float angle = Mathf.Atan2(xVel, zVel);
-        Debug.Log(angle);
-
-
-        float xVel2 = Mathf.Sin(angle);
-        float zVel2 = Mathf.Cos(angle);
-
-
-
-        this.transform.position += new Vector3(xVel2, 0, zVel2);
-        //Debug.Log(new Vector3(xVel2, 0, zVel2));
-    }
-    /*
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);      Wanted position
-        Vector3 direction = mousePosition - Player.transform.position;                     xxxxxxx
-
-        float angle = Mathf.Atan2(direction.y, direction.x);         angle
-        float b2 = Mathf.Sin(angle);
-        float a2 = Mathf.Cos(angle);
-
-        offset = new Vector3(a2, b2, -10f);
-        if (Peeking)
-        {
-            Vector3 desiredPosition = Player.transform.position + (lookLength * offset);
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpd);
-            transform.position = smoothedPosition;
-        }
-    */
 }
