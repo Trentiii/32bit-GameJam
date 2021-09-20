d14060bc79978b6c0d3b2e0320d235f77c1340bb
MUKILTEO\1707235 <1707235@mukilteo.wednet.edu>
2021-09-20 10:15:22 -0700

    main menu

---
+++ Assets/Thomas/c#/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenTheGame()
+    {
+        //SceneManager.LoadScene(1);
+        Debug.Log("starting...");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void credits()
+    {
+        //SceneManager.LoadScene(1);
+        Debug.Log("credits?");
+    }
+
+
+
+}
