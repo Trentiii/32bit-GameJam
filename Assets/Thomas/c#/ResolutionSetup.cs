685b0658e52de133fca731c4660957d14ac22ae2
MUKILTEO\1707224 <1707224@Mukilteo.wednet.edu>
2021-09-22 10:01:55 -0700

    ResolutionSetup

---
+++ Assets/Thomas/c#/ResolutionSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(320, 240, Screen.fullScreen);
+    }
+}
