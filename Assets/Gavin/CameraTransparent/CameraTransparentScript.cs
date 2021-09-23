ac7c4084ad5e5f4d4b35c9090a54027ca0fdca95
MUKILTEO\1707344 <1707344@mukilteo.wednet.edu>
2021-09-23 10:11:18 -0700

    Fixed lack of collision

--- Assets/Gavin/CameraTransparent/CameraTransparentScript.cs
+++ Assets/Gavin/CameraTransparent/CameraTransparentScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraTransparentScript : MonoBehaviour
 {
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,18 @@ public class CameraTransparentScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = new Ray(transform.position, transform.position - player.position);
+        RaycastHit hitInfo = new RaycastHit();
+        Physics.Raycast(ray, out hitInfo, Vector3.Distance(transform.position, player.position));
+
+        if(hitInfo.collider != null)
+        {
+
+        }
+
+        if(hitInfo.collider != null && hitInfo.collider.gameObject.tag == "Wall")
+        {
+            print("Player Hidden");
+        }
     }
 }
