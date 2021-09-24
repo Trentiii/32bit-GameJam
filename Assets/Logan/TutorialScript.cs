f5aa881d0d07c325fb3bd6586f3635aadd1f5a09
MUKILTEO\1707234 <1707234@mukilteo.wednet.edu>
2021-09-24 09:28:45 -0700

    Tutorial done

---
+++ Assets/Logan/TutorialScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenTutorial()
+    {
+        gameObject.SetActive(true);
+    }
+    public void CloseTutorial()
+    {
+        gameObject.SetActive(false);
+    }
+}
