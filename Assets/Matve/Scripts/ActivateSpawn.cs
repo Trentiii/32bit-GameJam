2f6ec61073df806a29340f87fa097a4b8ea93f81
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2021-09-24 09:34:57 -0700

    Spawner now linked with patrol

---
+++ Assets/Matve/Scripts/ActivateSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSpawn : MonoBehaviour
+{
+    public EnemyPatrol EP;
+    public AltSpawn2 AS2;
+    public GameObject spawnManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EP = gameObject.GetComponent<EnemyPatrol>();
+        spawnManager = GameObject.FindWithTag("SpawnM");
+        AS2 = spawnManager.GetComponent<AltSpawn2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EP.chasing)
+        {
+            AS2.spawning = true;
+        }
+    }
+}
