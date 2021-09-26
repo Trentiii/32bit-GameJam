c0412900fb39838da3085fffc5614631115f20b6
ChumaDaCabbage <1707224@mukilteo.wednet.edu>
2021-09-26 13:32:59 -0700

    Tutorial active on first startup, body size fixed

--- Assets/Logan/TutorialScript.cs
+++ Assets/Logan/TutorialScript.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class TutorialScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.SetActive(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OpenTutorial()
     {
         gameObject.SetActive(true);
---
+++ Assets/Scenes/MainScripts/TutorialStarter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialStarter : MonoBehaviour
+{
+    public GameObject tutorial;
+    static bool firstTime = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (firstTime)
+        {
+            tutorial.SetActive(true);
+            firstTime = false;
+        }
+    }
+}
